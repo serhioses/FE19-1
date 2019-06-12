d70e6211e411f6228b1af5fa83942a6e66b0e08d
serhioses <serega.mirosh@gmail.com>
2019-06-12 10:13:32 +0100

    js lesson 1

---
+++ public/views/javascript/lesson-1/js/homework-sample.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Unit link - https://learn.javascript.ru/variable-names
+
+// Task 1
+var myVariableName;
+var mySecondVariableName;
+
+// Additional task 1
+var myNiceVariableName;
+var myClearVariableName;
+var myAccurateVariableName;
---
+++ public/views/javascript/lesson-1/js/script.js
@@ -0,0 +1,112 @@
+'use strict';
+
+// var PI = 3.14;
+// var MAX_VAL = 140;
+
+// Primitives
+// var num = -0.4565;
+// var str = 'Text goes here...';
+// var noValue = null;
+// var notDefined = undefined;
+// var bool = true;
+
+// num = bool;
+
+// console.log(num);
+// console.log(str);
+// console.log(noValue);
+// console.log(notDefined);
+// console.log(bool);
+
+// Objects
+// var user = {
+//   name: 'Vasya',
+//   age: 45,
+//   married: true,
+//   cashe: 0,
+//   status: undefined,
+//   friend: {
+//     name: 'Petya',
+//     age: 68,
+//   },
+// };
+// user.age = 50;
+// console.log(user);
+var users = [
+  {
+    name: 'Vasya',
+    age: 45,
+    married: true,
+    cashe: 0,
+    status: undefined,
+  },
+  {
+    name: 'Vasya 2',
+    age: 80,
+    married: true,
+    cashe: 0,
+    status: undefined,
+  },
+  {
+    name: 'Vasya 3',
+    age: 98,
+    married: true,
+    cashe: 0,
+    status: undefined,
+  },
+  {
+    name: 'Vasya 4',
+    age: 111,
+    married: true,
+    cashe: 0,
+    status: undefined,
+  }
+];
+// users[0] = 50;
+// console.log(users);
+// function getAge (user) {
+//   return user.age;
+// }
+// console.log(getAge({ name: 'Vasya', age: 47 }));
+// console.dir(new Date());
+// console.dir(/[d]/);
+
+// let a = 10;
+// const PI = 3.14;
+// a = 20;
+// PI = 50;
+// console.log(a);
+// console.log(PI);
+// const user = { age: 80 };
+// console.log(user);
+
+// HOMEWORK SAMPLE
+// Unit link - https://learn.javascript.ru/variable-names
+
+// Task 1
+// your solution or explanation
+
+// Task 2
+// your solution or explanation
+
+// Task 3
+// your solution or explanation
+
+// Unit link - https://learn.javascript.ru/types-intro
+
+// Task 1
+// your solution or explanation
+
+// Task 2
+// your solution or explanation
+
+// Task 3
+// your solution or explanation
+
+// Additional task 1
+// your solution or explanation
+
+// Additional task 2
+// your solution or explanation
+
+// And so on...
