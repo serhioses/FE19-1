e2014289533a295c42fe6fc6ceb0dd890d0a85d9
serhioses <serega.mirosh@gmail.com>
2019-06-15 09:51:10 +0300

    js lesson 1

---
+++ public/views/javascript/lesson-2/js/script.js
@@ -0,0 +1,138 @@
+'use strict';
+
+// typeof
+// var user = undefined;
+// console.log(typeof user);
+
+// Unary, binary, operand
+// console.log(5 + 8 - 454 + 4654);
+// console.log(10 - 5 * 2);
+// console.log(8 * 11 / 5454 + 1);
+// console.log(154 / 45);
+// console.log((10 - 5) * (8 - 4) / 5);
+// console.log('20' * 5);
+// console.log('8' / 4);
+// console.log(50 - '12' * 2);
+
+// Strings addition
+// string + anything = string (exception)
+// var result = 'test' + ' sfksd;fk' + ' 44654' + '[]';
+// var sum = 10 + '11' + 1;
+// console.log(result);
+// console.log(sum);
+
+// Unary plus
+// console.log(+(1 - 5));
+// console.log(+'10');
+
+// Unary minus
+// console.log(-10 - -5);
+
+// Arithmetic operators
+
+// Priority (precedence)
+
+// Assignment
+// var a, b, c;
+// c = 10;
+// console.log(2 + 2);
+// a = 2 + (b = c + 10);
+// console.log(a);
+
+// Remainder
+// console.log(4 % 3);
+
+// Increment/Decrement
+// var counter = 0;
+// counter = (counter++) - (counter++);
+// counter--;
+// counter++ + 1;
+// counter = ++counter + 1;
+// --counter;
+
+// console.log(counter);
+
+// Arithmetic shortcuts
+// var num = 10;
+// num += 1;
+// console.log(num);
+
+// Comma operator
+// var c;
+// var a = (10, c = 20, 654564);
+// console.log(a, c);
+
+// Comparison operators
+
+// Boolean result
+
+// Number comparison
+// console.log(10 > (5 + 5));
+// console.log(10 < 5);
+// console.log(20 >= 20);
+// console.log(30 <= 40);
+// String comparison
+// console.log(+'3' > +'14');
+// console.log('ё' > 'я');
+
+// console.log(5 == 5);
+// console.log(5 != 5);
+
+// Comparison of different types
+// console.log(+'5' + +'10');
+// console.log(5 == '5');
+// console.log(0 == '');
+// console.log(null <= '');
+// console.log(0 == true);
+// console.log(true == 1);
+// console.log(0 == false);
+// console.log(0 != false);
+// console.log(true != 1);
+// console.log('' == []);
+// console.log(1 == [1]);
+
+// Equality (strict comparison)
+// console.log('5' === '5');
+// console.log(0 === '');
+// console.log(0 === false);
+// console.log(0 !== false);
+
+// console.log(0 === !0);
+// console.log(!(0 === 0));
+// console.log(!!0);
+// 0 -> false -> true -> false
+
+// Null and Undefined
+// console.log(null === undefined);
+// console.log(null == undefined);
+// console.log(null == null);
+// console.log(undefined == undefined);
+// console.log(null >= 0);
+// console.log(null <= 0);
+// console.log(null == 0);
+// console.log(null === 0);
+// var val = null;
+// console.log(val == null);
+
+// Null and 0
+// console.log(null);
+
+// Undefined
+// console.log(undefined >= 0);
+// console.log(undefined == 0);
+
+
+// Alert, prompt, confirm
+// var result = alert({ name: 'My name' });
+// console.log({ name: 'My name' });
+// var result = prompt('Enter any number', '');
+// var b = prompt('Enter any number 2', '');
+// console.log(result == 0);
+// var result = confirm('Confirm you are over 18');
+// console.log(result);
+
+// HOMEWORK
+// var a = 2;
+// var b = 4;
+
+// var c = (b += 5) - a++ * b - 3 + (b -= 3 + 1 - --a);
